ddd4f399cdfa852169fe4c79e8a65ff342b45969
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-10 22:11:41 +0700

    add new logo

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,14 +1,10 @@
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-interface LavaHeadProps {
-  size: number;
-}
-
-export default function LavaHead({ size }: LavaHeadProps) {
+export default function LavaHead({ size }: { size: number }) {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center align-middle leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <Image src="/logo.png" height={size} width={size} alt="LavaHead logo" />
       <p className="text-[44px]">LavaHead</p>
