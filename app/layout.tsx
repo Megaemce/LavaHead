5f96fd86e791d7f9144fc17721f2f6d3b4906a88
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-15 16:19:06 +0700

    add metadata content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | LavaHead',
+    default: 'LavaHead',
+  },
+  description: 'Track your mood and find the balance',
+  metadataBase: new URL('https://lavahead.vercel.app'),
+};
 export default function RootLayout({
   children,
 }: {
